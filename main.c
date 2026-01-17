4cb4fd4ca283a0256d957c951e542d94706311a8
ChangeWithTanmay <tanmaydas24pgs@gmail.com>
2026-01-18 01:54:41 +0530

    first commit

---
+++ main.c
@@ -0,0 +1,75 @@
+#include<stdio.h>
+#include <stdbool.h>
+
+int main(){
+    /*
+    // variable = A reusable container for a value.
+    //            Behaves as if it ware the value it contains.
+
+    // int = whole numbers (4 bytes in modern system)
+    // float = single-precision decimal number (4 bytes)
+    // double = double-presision decimal number (8 bytes)
+    // char = single character (1 bytes)
+    // char[] = array of characters (size varies)
+    // bool = true or false (1 byte, requires, <stdbool.h>)
+
+
+    
+
+    // Programming //
+
+
+    float gpa = 2.5;
+    float price = 19.99;
+    float temperature = -10.1;
+
+    // Float store/contain before point(.) 6 values
+
+    printf("Your gpa is %.2f\n", gpa); // 2.50
+    printf("the price is $%.f\n", price); // 19.990000
+
+
+
+
+    // dubble
+    double pi = 3.14159265358979;
+    double e = 2.7182818284590;
+    printf("The value of pi is %lf\n", pi); // 3.141593
+    // but
+    printf("The value of pi is %.15lf\n", pi); // 3.141592653589790
+    printf("The value of e is %.15lf\n", e); // 2.718281828459000
+    
+
+
+    char grade = 'A';
+    char symbol = '%';
+
+    printf("Your grade is %c\n", grade);
+    printf("Your Symbol is %c\n", symbol);
+    
+
+    char name[]="Brocode";
+    char food[] = "Pizza";
+
+    printf("hello, %s \n", name);
+    printf("Your favorite food is %s \n", food);
+    
+
+    bool isOnline = true; // 1
+    bool isPassed = false; // 0
+
+    printf("You are online: %d\n", isOnline);
+    printf("You are offline: %d\n", isPassed);
+
+    if (isOnline)
+    {
+        printf("You are online at 12pm");
+    } else{
+        printf("You ware sleep");
+    }
+    
+    */
+
+
+    return 0;
+}
