ac60f4085f7040d36c0acd4241c30a79edb6d28a
ChangeWithTanmay <tanmaydas24pgs@gmail.com>
2026-01-18 19:49:19 +0530

    homework is complete

--- homework.md
+++ homework.md
@@ -36,3 +36,49 @@ int main()
 }
 ```
 
+## MAD LIBS GAME
+
+```c
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+   // MAD LIBS GAME
+
+   char noun[50] = "";
+   char verb[50] = "";
+   char abjective1[50]="";
+   char abjective2[50]="";
+   char abjective3[50]="";
+
+   printf("Enter an adjective (description): ");
+   fgets(abjective1, sizeof(abjective1), stdin);
+   abjective1[strlen(abjective1)-1]='\0';
+
+   printf("Enter a noun (animal or person):");
+   fgets(noun, sizeof(noun), stdin);
+   noun[strlen(noun)-1]='\0';
+
+   printf("Enter an adjective (description): ");
+   fgets(abjective2, sizeof(abjective2), stdin);
+   abjective2[strlen(abjective2)-1]='\0';
+
+   printf("Enter a verb (ending w/ -ing): ");
+   fgets(verb, sizeof(verb), stdin);
+   verb[strlen(verb)-1]='\0';
+
+   printf("Enter an adjective (description): ");
+   fgets(abjective3, sizeof(abjective3), stdin);
+   abjective3[strlen(abjective3)-1]='\0';
+
+   printf("\nToday, I went to a %s zoo\n", abjective1);
+   printf("In an exhibit, I saw a %s\n",  noun);
+   printf("%s was %s and %s!\n", noun, abjective2, verb);
+   printf("I was %s!\n", abjective3);
+
+   return 0;
+}
+```
+
+## 
--- main.c
+++ main.c
@@ -3,30 +3,38 @@
 
 int main()
 {
-   // SOPPING CART PROGRAM
+   // MAD LIBS GAME
 
-   char item[50]="";
-   float price = 0.0;
-   int quantity=0;
-   char currency='$';
-   float total=0.0f;
+   char noun[50] = "";
+   char verb[50] = "";
+   char abjective1[50]="";
+   char abjective2[50]="";
+   char abjective3[50]="";
 
-   printf("What item would you like to buy?:");
-   
-   fgets(item, sizeof(item), stdin);
-   item[strlen(item)-1]='\0';
+   printf("Enter an adjective (description): ");
+   fgets(abjective1, sizeof(abjective1), stdin);
+   abjective1[strlen(abjective1)-1]='\0';
 
-   printf("What is the the price for each?:");
-   scanf("%f", &price);
-   printf("How many would you like?:");
-   scanf("%d", &quantity);
+   printf("Enter a noun (animal or person):");
+   fgets(noun, sizeof(noun), stdin);
+   noun[strlen(noun)-1]='\0';
 
-   total=price*quantity;
+   printf("Enter an adjective (description): ");
+   fgets(abjective2, sizeof(abjective2), stdin);
+   abjective2[strlen(abjective2)-1]='\0';
 
-   printf("You have bought %d %s\n", quantity, item);
+   printf("Enter a verb (ending w/ -ing): ");
+   fgets(verb, sizeof(verb), stdin);
+   verb[strlen(verb)-1]='\0';
 
-   printf("%c%.2f", currency, total);
+   printf("Enter an adjective (description): ");
+   fgets(abjective3, sizeof(abjective3), stdin);
+   abjective3[strlen(abjective3)-1]='\0';
 
+   printf("\nToday, I went to a %s zoo\n", abjective1);
+   printf("In an exhibit, I saw a %s\n",  noun);
+   printf("%s was %s and %s!\n", noun, abjective2, verb);
+   printf("I was %s!\n", abjective3);
 
    return 0;
 }
