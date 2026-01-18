d254e14170c85973ba9ea0501f76b3e669fe6c4d
ChangeWithTanmay <tanmaydas24pgs@gmail.com>
2026-01-18 20:58:15 +0530

    CIRCLE CALCULATOR PROGRAM

--- homework.md
+++ homework.md
@@ -81,4 +81,33 @@ int main()
 }
 ```
 
-## 
+## CIRCLE CALCULATOR PROGRAM
+```c
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    
+   // CIRCLE CALCULATOR PROGRAM
+
+   double radius = 0.0;
+   double area = 0.0;
+   double serface = 0.0;
+   double volume = 0.0;
+   const double PI = 3.14159;
+
+   printf("Enter the radius: ");
+   scanf("%lf", &radius);
+
+   area = PI * pow(radius, 2);
+   serface = 4 * PI * pow(radius, 2);
+   volume = (4.0/3.0) * PI * pow(radius, 3);
+
+   printf("Area: %.2lfcm\n", area);
+   printf("Surface area: %.2lfcm\n", serface);
+   printf("Volume: %.2lfcm", volume);
+
+   return 0;
+}
+```
--- main.c
+++ main.c
@@ -1,32 +1,26 @@
 #include <stdio.h>
 #include <math.h>
-#include <stdlib.h>
 
 int main()
 {
-   int x = 2;
-   float x=3.14;
+   // CIRCLE CALCULATOR PROGRAM
+   
+   double radius = 0.0;
+   double area = 0.0;
+   double serface = 0.0;
+   double volume = 0.0;
+   const double PI = 3.14159;
 
+   printf("Enter the radius: ");
+   scanf("%lf", &radius);
 
-   x = sqrt(x); // 9 -> 3
-   x = pow(x, 4); // 2^4 -> 16
+   area = PI * pow(radius, 2);
+   serface = 4 * PI * pow(radius, 2);
+   volume = (4.0/3.0) * PI * pow(radius, 3);
 
-   x = round(x); // 3.14 -> 3.000000
-   x = ceil(x); // 3.14 -> 4.000000
-   x = floor(x); // 3.14 -> 3.000000
-
-   /* Use #include <stdlib.h> */
-   int x = -3;
-   x = abs(x); // -3 -> 3 but use #include <stdlib.h>
-
-   float x = 45;
-
-   x=log(x); // 3 -> 1.098612
-   x = sin(x); // 45 -> 0.850904
-   x = cos(x); // 45 -> 0.525322
-   x = tan(x); // 45 -> 1.619775
-
-   printf("%f", x); // 45 -> 0.850904
+   printf("Area: %.2lfcm\n", area);
+   printf("Surface area: %.2lfcm\n", serface);
+   printf("Volume: %.2lfcm", volume);
 
    return 0;
 }
