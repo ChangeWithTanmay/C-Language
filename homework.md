b43a43cf39ced77847b0563db47e0816333f7f66
ChangeWithTanmay <tanmaydas24pgs@gmail.com>
2026-01-19 16:26:29 +0530

    WEIGHT CONVERTER PROGRAM

--- homework.md
+++ homework.md
@@ -152,4 +152,45 @@ int main()
 }
 ```
 
-## 
+## WEIGHT CONVERTER PROGRAM
+
+```c
+#include <stdio.h>
+
+int main()
+{
+   // WEIGHT CONVERTER PROGRAM
+
+   int choice = 0;
+
+   float pounds = 0.0f;
+   float kilograms = 0.0f;
+
+   printf("Weigth Conversion Calculator\n");
+   printf("1. Kilograms to Pounds\n");
+   printf("2. Pounds to kilograms\n");
+   printf("Enter your choice (1 or 2): ");
+   scanf("%d", &choice);
+
+   if(choice == 1){
+      // Kilogram to pounds
+      printf("Enter the weight in Kilogram: ");
+      scanf("%f", &kilograms);
+      pounds = kilograms * 2.20462;
+      printf("%.2f kilogram is equal to %.2f pounds\n", kilograms, pounds);
+   }
+   else if (choice == 2)
+   {
+      // Pounds to Kilograms
+      printf("Enter the weight in pounds: ");
+      scanf("%f", &pounds);
+      kilograms = pounds / 2.20462;
+      printf("%.2f pounds is equal to %.2f kilogram\n", pounds, kilograms);
+   } else{
+      printf("Invalid choose, Please enter 1 or 2");
+   }
+   
+
+   return 0;
+}
+```
--- main.c
+++ main.c
@@ -1,22 +1,38 @@
 #include <stdio.h>
-#include <string.h>
 
 int main()
 {
-   char name[50] = "";
+   // WEIGHT CONVERTER PROGRAM
 
-   printf("Enter your name: ");
-   fgets(name, sizeof(name), stdin);
-   name[strlen(name) - 1] = '\0';
+   int choice = 0;
 
-   if (strlen(name) == 0)
-   {
-      printf("You did not enter your name");
+   float pounds = 0.0f;
+   float kilograms = 0.0f;
+
+   printf("Weigth Conversion Calculator\n");
+   printf("1. Kilograms to Pounds\n");
+   printf("2. Pounds to kilograms\n");
+   printf("Enter your choice (1 or 2): ");
+   scanf("%d", &choice);
+
+   if(choice == 1){
+      // Kilogram to pounds
+      printf("Enter the weight in Kilogram: ");
+      scanf("%f", &kilograms);
+      pounds = kilograms * 2.20462;
+      printf("%.2f kilogram is equal to %.2f pounds\n", kilograms, pounds);
    }
-   else
+   else if (choice == 2)
    {
-      printf("Hello %s", name);
+      // Pounds to Kilograms
+      printf("Enter the weight in pounds: ");
+      scanf("%f", &pounds);
+      kilograms = pounds / 2.20462;
+      printf("%.2f pounds is equal to %.2f kilogram\n", pounds, kilograms);
+   } else{
+      printf("Invalid choose, Please enter 1 or 2");
    }
+   
 
    return 0;
 }
